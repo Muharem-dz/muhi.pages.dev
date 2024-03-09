cf54eefad6f9a16514642dcc6198418020950821
Muharem-dz <dzafichary@gmail.com>
2024-03-09 17:12:45 +0100

    k

--- script.js
+++ script.js
@@ -50,19 +50,3 @@ window.ontouchend = (e) => handleOnUp(e.touches[0]);
 window.onmousemove = (e) => handleOnMove(e);
 
 window.ontouchmove = (e) => handleOnMove(e.touches[0]);
-
-const toggle = document.getElementById("toggleDark");
-const body = document.querySelector("body");
-
-toggle.addEventListener("click", function () {
-  this.classList.toggle("bi-moon");
-  if (this.classList.toggle("bi-brightness-high-fill")) {
-    body.style.background = "white";
-    body.style.color = "black";
-    body.style.transition = "2s";
-  } else {
-    body.style.background = "black";
-    body.style.color = "white";
-    body.style.transition = "2s";
-  }
-});
